4d4b276f3ab2666f00bb096d97efc3912ef36553
mquang2002 <mquang2022002@gmail.com>
2024-08-29 22:42:57 +0700

    feact: load env - connect to db - setting dockercompose

--- config/config.go
+++ config/config.go
@@ -1,5 +1,33 @@
 package config
 
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+)
+
 // create struct for env
+type DB struct {
+	DBHOST     string `envconfig:"DB_HOST"`
+	DBUSER     string `envconfig:"DB_USER"`
+	DBPASSWORD string `envconfig:"DB_PASSWORD"`
+	DBNAME     string `envconfig:"DB_NAME"`
+	PORT       int    `envconfig:"PORT"`
+}
 
 // load env here
+func LoadConfig() (DB, error) {
+	var cfg DB
+	//load env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+	//fill env data to struct
+	err = envconfig.Process("myapp", &cfg)
+	if err != nil {
+		log.Fatalf("process env failed, error: %v", err)
+	}
+	return cfg, nil
+}
--- repository/repo.go
+++ repository/repo.go
@@ -1,3 +1,24 @@
 package repository
 
+import (
+	"fmt"
+
+	"github.com/quocbang/pet-project/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
 // return client by *gorm.Client
+func ConnectToDatabase(cfg config.DB) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Shanghai",
+		cfg.DBHOST, 
+		cfg.DBUSER, 
+		cfg.DBPASSWORD, 
+		cfg.DBNAME, 
+		cfg.PORT)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
